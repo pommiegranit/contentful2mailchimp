2ead486a66b04914fe10ceeccca3a6eb0ad2df23
Chris Knowles <knowles.chris.d@gmail.com>
2018-11-28 22:00:10 +1100

    Add config

---
+++ config.py
@@ -0,0 +1,11 @@
+CONTENTFUL_SPACE_ID = '<your space id>'
+CONTENTFUL_ACCESS_TOKEN = '<your access token>'
+
+MAILCHIMP_API_KEY = '<your api key>'
+MAILCHIMP_USER = '<your mailchimp user id>'
+
+CAMPAIGN_LIST_ID = '<your campaign list id'>
+CAMPAIGN_TEMPLATE_ID = <your template id>
+CAMPAIGN_FOLDER_ID = '<your folder id>'
+CAMPAIGN_REPLY_TO = '<your reply-to email address'
+CAMPAIGN_TEST_EMAILS = [<array of test email addresses>]
